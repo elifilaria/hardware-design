eb3369ed26d0375674ce69e6bae6084a46463d8e
Elif I Yurtseven <yurtse1@stolaf.edu>
2017-09-21 16:48:59 -0500

    hw5 C complete: facts.cpp

---
+++ hw5/facts.cpp
@@ -0,0 +1,27 @@
+/* print factorials in C++ language.  R. Brown, 9/2010 */
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+long factorial(int n){
+  long ans = 1;
+  int i=1;
+  while (i<=n){
+     ans = ans*i;
+     i++;
+  }
+  return ans;
+ }
+int main()
+{
+  cout << "n\tn!" << endl << "----------------" << endl;
+  int n=0;
+  while (n<=5){
+    cout << n << "\t" << factorial(n) << endl;
+    n++;
+  }
+  return 0;
+}
+
