6cfead3751e2fdc7234c7f207ed289a850a0a4ff
Elif I Yurtseven <yurtse1@stolaf.edu>
2017-10-04 14:36:03 -0500

    HW12 E1 complete: iterators.py

---
+++ hw12/iterators.py
@@ -0,0 +1,12 @@
+for c in "abcde":
+    print(c)
+
+for n in ["John", "Jacob", "Jingle", "Heimer", "Schmidt"]:
+    print (n)
+
+for x in (1, "red", 2, "green"):
+    print(x)
+
+
+for nx in ("I am", 18, "+", 3, "years", "old"):
+    print(nx)
