8d8cee8e7bd1773d81ce4e2643f77b64267ef66a
Elif I Yurtseven <yurtse1@stolaf.edu>
2017-11-05 23:52:49 -0600

    PP-ALU basic assignment complete and commented : alu.cpp

--- pp-alu/alu.cpp
+++ pp-alu/alu.cpp
@@ -2,34 +2,60 @@
 #include "blogic.H"
 using namespace std;
 
-int alu (bool F_0, bool F_1, bool INVA, bool ENA, bool ENB, bool carry_in, bool A, bool B)
+int alu (bool f0, bool f1, bool inva, bool ena, bool enb, bool c_i, bool a, bool b)
 {
-	bool output = bor(band(band(band(B, ENB), bxor(band(A, ENA), INVA)), band(bnot(F_0), bnot(F_1))), 
-			band(bor(bxor(band(A, ENA), INVA), band(B, ENB)), band(bnot(F_0), F_1)), 
-			band(bnot(band(B, ENB)), band(F_0, bnot(F_1))), 
-			band(bxor(carry_in, bxor(band(B, ENB), bxor(band(A, ENA), INVA))), band(F_0, F_1)));
+	bool output = bor(band(band(band(b, enb), bxor(band(a, ena), inva)), band(bnot(f0), bnot(f1))), 
+			band(bor(bxor(band(a, ena), inva), band(b, enb)), band(bnot(f0), f1)), 
+			band(bnot(band(b, enb)), band(f0, bnot(f1))), 
+			band(bxor(c_i, bxor(band(b, enb), bxor(band(a, ena), inva))), band(f0, f1)));
 
-	bool carry_out = bor(band(band(F_0, F_1), bxor(band(A, ENA), INVA), band(B, ENB)), 
-			band(band(F_0, F_1), bxor(band(B, ENB), bxor(band(A, ENA), INVA)), carry_in));
+	bool carry_out = bor(band(band(f0, f1), bxor(band(a, ena), inva), band(b, enb)), 
+			band(band(f0, f1), bxor(band(b, enb), bxor(band(a, ena), inva)), c_i));
 
 
 	int result;
-	if (carry_out == 0 && output == 0)
-		result = 0;
+	if (carry_out == 0 && output == 1)
+		result = 2;
 	else if (carry_out == 1 && output == 1)
 		result = 3;
+	else if (carry_out == 1 && output == 0)
+		result = 1;
 	else
-		result = 2;
+		result = 0;
 	return result;
 
 } 
 
+void print_call (bool f0, bool f1, bool inva, bool ena, bool enb, bool c_i, bool a, bool b)
+{
+	cout << "alu(" << f0 << ", " << f1 << ", " << inva << ", " << ena << ", " << enb << ", " << c_i << ", " << a << ", " << b << ") --> " << alu(f0, f1, inva, ena, enb, c_i, a, b) << endl;
+	
+}
+
+void print_section (bool f0, bool f1, bool inva, bool ena, bool enb)
+{
+	cout << "F0=" << f0 << ", " << "F1=" << f1 << ", " << "INVA=" << inva << ", " << "ENA=" << ena << ", " << "ENB=" << enb << endl;
+    for (int c_i = 0; c_i <= 1; c_i++)
+    	for (int a = 0; a <= 1; a++)
+    		for (int b = 0; b <= 1; b++)
+                print_call(f0, f1, inva, ena, enb, c_i, a, b);
+
+
+}
+
+void print_section_32 ()
+{
+    for (int f0 = 0; f0 <= 1; f0++)
+        for (int f1 = 0; f1 <= 1; f1++)
+            for (int inva = 0; inva <= 1; inva++)
+                for (int ena = 0; ena <= 1; ena++)
+                    for (int enb = 0; enb <= 1; enb++){
+                        print_section(f0, f1, inva, ena, enb);
+                        cout << endl;
+                    }
+}
 int main()
 {
-	int input = alu(0, 0, 0, 1, 1, 0, 1, 0);
-	cout << input << endl;
-	int input2 = alu(1, 1, 0, 1, 1, 1, 1, 1);	
-	cout << input2 << endl;
-	int input3 = alu(0, 1, 1, 0, 1, 0, 0, 1);
-	cout << input3 << endl;
+	print_section_32();
+
 }
