eb85470f1240e1ee914f3cd0c894cf83bdc1e5fd
yurtse1 <yurtse1@stolaf.edu>
2017-12-01 16:03:09 -0600

    HW32 fixed echolcb code now it works perfectly : echolcb.s

--- hw32/echolcb.s
+++ hw32/echolcb.s
@@ -1,5 +1,4 @@
-!!! copy standard input to standard output, skipping spaces
-!!! R. Brown, 11/05
+!!! changes capital input letter to corresponding lower case letter in the output
 
 	_EXIT = 1
 	_READ = 3
@@ -9,8 +8,6 @@
 	_GETCHAR = 117
 
 	BUFFSIZE = 80
-
-
 .SECT .TEXT
 start:
 	PUSH	endpr-prompt	! print prompt
@@ -20,33 +17,23 @@ start:
 	SYS
 	ADD	SP,8		! clean up stack
 
-	PUSH 	1
+	PUSH	1	! read input line
 	PUSH	buff
 	PUSH	_STDIN		! from standard input
 	PUSH	_GETCHAR
 	SYS
 	ADD	SP,2		! clean up stack
-
-	MOVB 	(buff+1), 10
-
-	PUSH 	1
-	MOVB 	(BX), AL
-	ADDB	(BX), 32
+	MOVB 	(buff), AL
+	ADDB 	(buff),32
+	PUSH 	endstr-str
 
 
-	PUSH 	endpr2-prompt2
-	PUSH 	prompt2
+byte:
+	PUSH 	 str 
 	PUSH 	_STDOUT
 	PUSH 	_WRITE
 	SYS
 	ADD SP,8
-	PUSH 	1
-	PUSH 	BX	
-	PUSH 	_STDOUT
-	PUSH 	_WRITE
-	SYS
-	ADD SP, 8
-
 
 	PUSH 	0
 	PUSH 	_EXIT
@@ -56,12 +43,13 @@ start:
 .SECT .DATA
 prompt:	.ASCII "Enter an upper case letter: "
 endpr:
-prompt2: .ASCII "The lower-case equivalent is: "
-endpr2:
+str: .ASCII "The lower-case equivalent is: "
+buff:
+	.BYTE 0
+	.ASCII "\n"
+endstr:
 size:	.WORD	0		! length of input
 
 .SECT .BSS
-buff:	.SPACE 2
-.SECT .BSS
-endbuff:
+
 	
